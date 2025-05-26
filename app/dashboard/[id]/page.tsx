d2510281ac11ac437d403fbe2a4321d74360a179
J-S-Nafeez <jammalamadugushaiknafeez@gmail.com>
2025-05-26 19:59:59 +0530

    Done

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,19 +1,15 @@
 // app/dashboard/[id]/page.tsx
 
-import React from 'react';
-
 interface PageProps {
   params: {
     id: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
+export default function Page({ params }: PageProps) {
   return (
     <div>
-      <h1>Dashboard for ID: {params.id}</h1>
+      <h1>Dashboard Page for ID: {params.id}</h1>
     </div>
   );
-};
-
-export default Page;
+}
